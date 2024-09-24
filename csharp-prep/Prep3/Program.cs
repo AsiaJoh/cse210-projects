8fc59f8dba8b92e3098fe2302726108266589f4c
AsiaJoh <joh22056@byui.edu>
2024-09-24 14:24:34 -0600

    fnished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,6 +5,33 @@ class Program
     static void Main(string[] args)
     {
         // Guessing Number Game
-        
+       
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1,101);
+
+        Console.WriteLine("A magic secret number has been generated.");
+
+        string strGuessNum = "";
+        int guessNum = 0;
+
+        while (magicNum != guessNum)
+        {
+            Console.Write("What is your guess? ");
+            strGuessNum = Console.ReadLine();
+            guessNum = int.Parse(strGuessNum);
+
+            if (magicNum > guessNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNum < guessNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } 
     }
 }
