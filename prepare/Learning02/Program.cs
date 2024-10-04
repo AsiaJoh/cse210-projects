7188c9a4b0bc614adc9518d6a146ab3f25956b7b
AsiaJoh <joh22056@byui.edu>
2024-10-03 19:25:02 -0600

    Progress

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,11 +6,20 @@ class Program
     {
         Job job1 = new Job();
 
-        job1._company = "";
+        job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
-        job1._startYear = 0;
-        job1._endYear = 0;
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
-        Console.WriteLine(job1._jobTitle);
+        job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        job2.DisplayJobDetails();
     }
 }
