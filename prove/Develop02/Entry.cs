4c173b46bb78b7fd650dd2490d66ed42322f9e8b
AsiaJoh <joh22056@byui.edu>
2024-10-08 13:28:44 -0600

    Making progress

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Entry
+{
+    public string _date; // I believe (public?) class variables (or attributes) like this have an _ in front of their names
+    public string _prompt;
+    public string _content;
+
+    // Now for the Class's functions (or the methods)
+
+    public void DisplayEntry() // (Public?) Class functions (or methods) like this use TitleCase for their names
+    {
+        // Display the user's journal entries of the past and present(?)
+        Console.Write("> ");
+        string journalEntry = Console.ReadLine();
+
+    }
+
+    public void SaveEntry()
+    {
+        // Save the user's journal entry to a file
+
+        // Prompt for the file name
+        // Save to that file
+    }
+}
