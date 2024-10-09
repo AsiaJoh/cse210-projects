f7a321463d8f53452b611829d7e5665f93a67ff9
AsiaJoh <joh22056@byui.edu>
2024-10-09 12:26:00 -0600

    Frustration

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,20 +7,20 @@ class Program
     {
         int keepProgramOpen = 0;
 
-        Console.WriteLine("Welcome to the Journal Program!");
-        Console.WriteLine("Please select one of the following choices (via number):");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-        Console.Write("What would you like to do? ");
-        string stringOption = Console.ReadLine();
-
-        int option = int.Parse(stringOption);
-
         while (keepProgramOpen == 0)
         {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices (via number):");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            string stringOption = Console.ReadLine();
+
+            int option = int.Parse(stringOption);
+
             if (option == 1)
             {
                 // Start the process of writing a new entry
