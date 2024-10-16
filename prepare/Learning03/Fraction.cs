0d14b4e790c2bf274173adbb9b53e60e6acb9ebc
AsiaJoh <joh22056@byui.edu>
2024-10-15 21:19:48 -0600

    Progressssss

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,6 +1,70 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Fraction
 {
-    
+    int _top; // When you don't specify the variable as public (by putting the keyword before the statement), the variable is automatically private
+    int _bottom; // However, if you wanted to specify in in code, you would write this-> private int _bottom;
+    // Things that are private can only be directly accessed within their class
+
+    // Our constructors!
+    // Turns out, constructors are just what help us make objects/instantiations of the class.
+    // They can help the object be made with default parameters, and/or optional parameters
+    // The constructors specify all the ways the object can be made, and you tell it is an isntructor (instead of a function) because there is no defined return type, not even void
+    // Constructors must have the same title as the class(?)
+
+    // We want these constructors to be public(?)
+    public Fraction() 
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber) 
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom) 
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Now for the methods (specifically the getters and setters)
+    public void GetTop() // Methods are also automatically private (I think), so we must specify these to be public
+    {
+        int top = _top;
+        Console.WriteLine($"Here is the top: {top}");
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void GetBottom()
+    {
+        int bottom = _bottom;
+        Console.WriteLine($"Here is the bottom: {bottom}");
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    // The rest of the methods
+    public string GetFractionString()
+    {
+        string fraction = $"{_top}/{_bottom}";
+        return fraction;
+    }
+
+    public double GetDecimalValue()
+    {
+        double decimaal = (double)_top / _bottom; // The (double) ensures that even though we're passing in integers, the decimaal variable remains a double-type variable
+        return decimaal;
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,81 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Test-instancing time! 
+        Fraction fractionNone = new Fraction(); // Object 1 instance
+        Fraction fractionOne = new Fraction(5); // Object 2 instance
+        Fraction fractionTwo = new Fraction(2, 3); // Object 3 instance
+
+        int done = 0;
+        while (done != 1)
+        {
+            Console.WriteLine("0, 1, or 2 numbers?");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                Console.WriteLine("Gimme a number ");
+                int numberGiven = int.Parse(Console.ReadLine());
+                fractionNone.SetTop(numberGiven);
+                fractionNone.GetTop();
+
+                Console.WriteLine("Gimme a number ");
+                int numberGiven2 = int.Parse(Console.ReadLine());
+                fractionNone.SetBottom(numberGiven2);
+                fractionNone.GetBottom();
+
+                Console.WriteLine();
+                Console.WriteLine("A fraction and decimal: ");
+                Console.WriteLine(fractionNone.GetFractionString());
+                Console.WriteLine(fractionNone.GetDecimalValue());
+
+                Console.WriteLine();
+                Console.WriteLine("Done (0=no/1=yes)? ");
+                done = int.Parse(Console.ReadLine());
+
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("Gimme a number ");
+                int numberGiven = int.Parse(Console.ReadLine());
+                fractionOne.SetTop(numberGiven);
+                fractionOne.GetTop();
+
+                Console.WriteLine("Gimme a number ");
+                int numberGiven2 = int.Parse(Console.ReadLine());
+                fractionOne.SetBottom(numberGiven2);
+                fractionOne.GetBottom();
+
+                Console.WriteLine();
+                Console.WriteLine("A fraction and decimal: ");
+                Console.WriteLine(fractionOne.GetFractionString());
+                Console.WriteLine(fractionOne.GetDecimalValue());
+
+                Console.WriteLine();
+                Console.WriteLine("Done (0=no/1=yes)? ");
+                done = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Gimme a number ");
+                int numberGiven = int.Parse(Console.ReadLine());
+                fractionTwo.SetTop(numberGiven);
+                fractionTwo.GetTop();
+
+                Console.WriteLine("Gimme a number ");
+                int numberGiven2 = int.Parse(Console.ReadLine());
+                fractionTwo.SetBottom(numberGiven2);
+                fractionTwo.GetBottom();
+
+                Console.WriteLine();
+                Console.WriteLine("A fraction and decimal: ");
+                Console.WriteLine(fractionTwo.GetFractionString());
+                Console.WriteLine(fractionTwo.GetDecimalValue());
+
+                Console.WriteLine();
+                Console.WriteLine("Done (0=no/1=yes)? ");
+                done = int.Parse(Console.ReadLine());
+            }
+        }
     }
 }
