1e0833ba1f36cd0d4a2d54a4e9d592150f948be5
AsiaJoh <joh22056@byui.edu>
2024-10-24 15:40:22 -0600

    Now starting progress on the main program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        // If no input but not scripture fully redactedm (get fully redactedm ) rldr rnf
+        // If no input but not scripture fully redacted (get fully redacted ) rldr rnf
         // If times redacted = word count, set scripture fully redacted boolean to true
     }
 }
