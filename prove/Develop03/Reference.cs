a49bc41464d405d47f4faba6e87a67e41e072880
AsiaJoh <joh22056@byui.edu>
2024-10-24 14:23:28 -0600

    YiddlesYodles

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -6,21 +6,29 @@ public class Reference
     private int _chapter;
     private int _verseStart;
     private int _verseEnd;
+    private bool _multiVersesUsed = false;
 
     // Constructorssss
     public Reference(string book, int chapter, int start, int end)
     {
-
+        _multiVersesUsed = true;
     }
 
     public Reference(string book, int chapter, int start)
     {
-
+        _multiVersesUsed = false;
     }
 
     // Actual Methods
     public void DisplayReference()
     {
-        Console
+        if (_multiVersesUsed)
+        {
+            Console.WriteLine($"{_book} {_chapter}:{_verseStart}-{_verseEnd}");
+        }
+        else
+        {
+            Console.WriteLine($"{_book} {_chapter}:{_verseStart}");
+        }
     }
 }
