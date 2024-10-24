2ffb73a4000ac22bce449114efb62feeb04f3058
AsiaJoh <joh22056@byui.edu>
2024-10-24 15:02:19 -0600

    hdskjfhskjdfsk

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,11 +3,11 @@ using System;
 public class Scripture
 {   // The private piece is unnecessary, but for the sake of remembering syntax/format I'm going to write it this time
     private Reference _reference; 
-    private List<Word> _wordsList;
+    private List<Word> _wordList;
 
     public void DisplayScripture()
     {
-        _wordsList = new List<Word>
+        _wordList = new List<Word>
         {
             new Word("Behold,"), new Word("this"), new Word("is"), new Word("my"), new Word("work"), new Word("and"), new Word("my"), new Word("glory--"), 
         new Word("to"), new Word("bring"), new Word("to"), new Word("pass"), new Word("the"), new Word("immortality"), new Word("and"), new Word("eternal"), 
@@ -22,6 +22,10 @@ public class Scripture
         Console.WriteLine();
 
         // Display the scripture via printing out the funky list...
+        foreach (Word word in _wordList)
+        {
+            Console.Write(word );
+        }
     }
 
     public void RedactScripture()
@@ -31,16 +35,16 @@ public class Scripture
         Random random = new Random();
 
         // Find the length of the word list that is the scripture (index-wise)
-        int listLength = _wordsList.Count;
+        int listLength = _wordList.Count;
 
         // Generate a random integer within that list (according to it's indexes)
         int randomNumber = random.Next(0, listLength); // The first number is inclusive, the second exclusive, so the highest number shoulD be the max index
 
         // Check if the related word is redacted
-        if (!_wordsList[randomNumber].GetRedacted()) // Check if redacted status is false
+        if (!_wordList[randomNumber].GetRedacted()) // Check if redacted status is false
         {
             // Call the redact word method
-            _wordsList[randomNumber].RedactWord();
+            _wordList[randomNumber].RedactWord();
         }
         else // If the redacted status is true
         { 
