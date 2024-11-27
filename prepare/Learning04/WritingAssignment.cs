d5c505b7eee5c3dc4bfa09ee39ade89e43e0ed2d
AsiaJoh <joh22056@byui.edu>
2024-11-27 11:49:51 -0700

    Fin

--- prepare/Learning04/WritingAssignment.cs
+++ prepare/Learning04/WritingAssignment.cs
@@ -10,6 +10,8 @@ class WritingAssignment : Assignment
         _title = "Yiddles Yodles";
     }
 
+    // "Notice the syntax here that the WritingAssignment constructor has 3 parameters and then
+    // it passes 2 of them directly to the "base" constructor, which is the "Assignment" class constructor."
     public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
     {
         _title = title;
