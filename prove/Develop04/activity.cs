8acc47ec87c6cd410214d2bccbe027bc4c56b283
AsiaJoh <joh22056@byui.edu>
2024-12-05 15:53:32 -0700

    Made progress on beginning/ending messages - Next need to work on animation for ending message

--- prove/Develop04/activity.cs
+++ prove/Develop04/activity.cs
@@ -6,21 +6,15 @@ class Activity
     string _endMessage;
 
     //Constructor(s)
-    public Activity(string name, string description, int duration, string endMessage)
+    public Activity(string name)
     {
         // Assigning the values
         _name = name;
-        _description = description;
-        _duration = duration;
-        _endMessage = endMessage;
     }
-    public Activity(string name, string description, int duration, string endMessage, string time)
+    public Activity(string name, string time)
     {
         // Assigning the values
         _name = name;
-        _description = description;
-        _duration = duration;
-        _endMessage = endMessage;
         time;
     }
 
@@ -28,7 +22,19 @@ class Activity
     //Methods
     public void BeginningMessage() {
         // Put together the beginning message
-        string message = $"{}";
+        string message = $"Welcome to the {_name} Activity. \n\n{_description}";
+        Console.WriteLine(message);
+
+        string messageDuration = "How long, in seconds, would you like for your session? ";
+        Console.Write(messageDuration);
+        _duration = int.Parse(Console.ReadLine());
+    }
+
+    public void EndingMessage() {
+        // Put together the ending message
+        string message = $"Well done!! \n\nYou have completed {_duration} seconds of the {_name}.";
+        Console.WriteLine(message);
+        
     }
 
     public void Pause() {
