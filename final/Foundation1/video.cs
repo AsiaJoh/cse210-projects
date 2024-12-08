d3a478e64da4ee65f3b14c6aee42093720a5aba9
AsiaJoh <joh22056@byui.edu>
2024-12-07 17:57:30 -0700

    E

---
+++ final/Foundation1/video.cs
@@ -0,0 +1,18 @@
+class Video
+{
+    string _title;
+    
+
+    // Constructor(s)
+    public Video()
+    {
+        // Assigning the values
+        _title = title;
+    }
+
+    // Methods
+    public int CommentsCount() {
+        // Set timer for current activity
+        
+    }
+}
