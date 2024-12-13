2ea0d5da0f2dc98fb7b30ebdb56c740a8554d2b2
AsiaJoh <joh22056@byui.edu>
2024-12-12 18:36:05 -0700

    E

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Write a program that creates at least two orders with a 2-3 products each. 
+        
+
+
+
+
+        // Call the methods to get the packing label, the shipping label, and the total price of the order, and display the results of these methods.
     }
 }
